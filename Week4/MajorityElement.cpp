ff4dfd7c174bdfa5320cd042b5b73f800355e7e4
Harsh Singh <harshsngh07@gmail.com>
2020-05-31 17:40:25 +0530

    majority element

--- Week4/MajorityElement.cpp
+++ Week4/MajorityElement.cpp
@@ -1,29 +1,36 @@
-
 #include <bits/stdc++.h>
 using namespace std;
+
+void findMajority(long long int a[], long long int n)
+{
+    unordered_map<int, int> m;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        m[a[i]]++;
+    }
+
+    int count(0);
+
+    for (auto i : m)
+    {
+        if (i.second > n / 2)
+        {
+            count = 1;
+            cout << 1 << endl;
+            break;
+        }
+    }
+
+    if (count == 0)
+        cout << 0 << endl;
+}
 
 int main()
 {
     long long int n;
     cin >> n;
     long long int a[n];
-    for (int i = 0; i < n; i++)
-        cin >> a[i];
 
-    sort(a, a + n);
-
-    long long int count(1);
-
-    for (int i = 0; i < n - 1; i++)
-    {
-        if (a[i] == a[i + 1])
-            count++;
-    }
-
-    if (count > n / 2)
-        cout << 1 << endl;
-    else
-        cout << 0 << endl;
-
-    //cout << count;
+    findMajority(a, n);
 }
