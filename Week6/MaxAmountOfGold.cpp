5040bcc08786dbbdd4749c59273598291417b051
Harsh Singh <harshsngh07@gmail.com>
2020-07-06 17:48:20 +0530

    max amount of gold

---
+++ Week6/MaxAmountOfGold.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int KnapSack(int W, int wt[], int value[],int n)
+{
+    int i, w;
+    int K[n + 1][W + 1];
+
+    // Build table K[][] in bottom up manner
+    for (i = 0; i <= n; i++)
+    {
+        for (w = 0; w <= W; w++)
+        {
+            if (i == 0 || w == 0)
+                K[i][w] = 0;
+            else if (wt[i - 1] <= w)
+                K[i][w] = max(
+                    value[i-1]+ K[i - 1][w - wt[i - 1]],
+                    K[i - 1][w]);
+            else
+                K[i][w] = K[i - 1][w];
+        }
+    }
+
+    return K[n][W];
+}
+
+int main()
+{
+    int W,n;
+    cin >> W>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+        cin >> a[i];
+    
+    cout << KnapSack(W,a,a,n) << endl;
+}
